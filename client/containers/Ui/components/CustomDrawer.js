3b131f30bb2d56a8b642c864f6bbdd19ab2fbae3
SandroRybarik <sandro.rybarik@gmail.com>
2016-09-21 21:42:30 +0200

    adding MenuItemStyle, creating links from menu to navigate

--- client/containers/Ui/components/CustomDrawer.js
+++ client/containers/Ui/components/CustomDrawer.js
@@ -4,6 +4,17 @@ import MenuItem from 'material-ui/MenuItem';
 
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
+
+
+/**
+ * Component Styling
+ */
+
+const MenuItemStyle = {
+    color: '#333',
+    textDecoration: 'none'
+};
+
 const CustomDrawer = ({
     openDrawer,
     onDrawerToggle
@@ -17,9 +28,9 @@ const CustomDrawer = ({
             <img src="http://www.sthildas.qld.edu.au/wp-content/themes/hildas-2014/images/img-default-staff.png" />
             </CardMedia>
         </Card>
-        <MenuItem>Nove otazky</MenuItem>
-        <MenuItem>Nedokoncene</MenuItem>
-        <MenuItem>Deadline</MenuItem>
+
+        <MenuItem><a style={MenuItemStyle} href="/users/create">Vytvorit usera</a></MenuItem>
+        <MenuItem><a style={MenuItemStyle} href="/users">Zobrazit userov</a></MenuItem>
     </Drawer>
 
 )
